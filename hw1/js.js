a9a25c1273d6f8d5b2cc90d97b2e1b62c4935ae6
Mizutooo <130977348+Mizutooo@users.noreply.github.com>
2023-05-29 15:20:24 +0600

    Add files via upload

---
+++ hw1/js.js
@@ -0,0 +1,40 @@
+//1  mission
+
+// function printArrayValues(arr, i=0) {
+//     if (i >= arr.length) {
+//     return;
+//     }
+//     console.log(arr[i], 'array');
+//     printArrayValues(arr, i+1);
+//     }
+    
+//     printArrayValues([1, 2, 3]);
+
+
+//2 mission
+
+//     let str = "My name 457 is Alex";
+// let nonDigits = str.replace(/\d/g, ''); 
+// console.log(nonDigits); 
+
+
+//3 mission
+
+// let str = "Hello\tWorld\nTest";
+// let whitespace = str.match(/\s/g);
+// console.log(whitespace);
+
+
+//4 pensum
+
+// function countChar(str, char) {
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//     if (str[i] === char) {
+//     count++;
+//     }
+//     }
+//     return count;
+//     }
+    
+//     console.log(countChar('loremipsumdolor', 'o')); 
