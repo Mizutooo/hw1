393e1bed4d185b2710ed0e35c8beb80d988fec29
Mizutooo <130977348+Mizutooo@users.noreply.github.com>
2023-07-04 04:02:46 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+import Header from './progect/header';
+import './App.css';
+import './progect/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <Header/>
+      </>
+    </div>
+  );
+}
+
+export default App;
